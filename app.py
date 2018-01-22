3470d5ea69b0ae6fffc9c0fd3f11dfac278c4d33
sadidtahsin <tamim.tm@gmail.com>
2018-01-22 15:23:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def sensor(name=None,age=None):
       return "done " +name+" "+age
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+   port = int(os.environ.get('PORT', 5000))
+   app.run(host='0.0.0.0', port=port, debug=True)
