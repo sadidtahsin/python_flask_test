d01cc76dfa6404d24a6f31b3bf5770889b82fc9f
TAMIM_ <tamim.tm@gmail.com>
2018-01-22 15:00:06 +0600

    test

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request
+from pymongo import MongoClient
+app = Flask(__name__)
+#test
+client = MongoClient('localhost:27017')
+db=client.sensor
+
+print(client)
+print(db)
+print("done")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/sensor/<name>/<age>',methods = ['POST', 'GET'])
+def sensor(name=None,age=None):
+   if request.method == 'POST':
+      print (request.is_json)
+      print("hello\n")
+      content = request.json
+    #   db.value.insert_one(content)
+      print (content)
+      
+      return "done"
+   else:
+
+      return "done " +name+" "+age
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
