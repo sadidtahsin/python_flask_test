e144c69ca8678f3241def37d16905f3e5ba3d09b
sadidtahsin <tamim.tm@gmail.com>
2019-05-13 17:28:37 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     #msg={"text": {"text": ["We could find few matching products based on your query"]}},{"text": {"text": ["2nd text"]}}
-    msg={ "fulfillmentText": "This is a text response", "fulfillmentMessages": [], "source": "example.com", "payload": { "google": { "expectUserResponse": True, "richResponse": { "items": [ { "simpleResponse": { "textToSpeech": "This is a Basic Card:" } }, { "basicCard": { "title": "card title", "image": { "url": "https://cdn1.imggmi.com/uploads/2019/5/13/698a177fef054e42548734435de89db3-full.jpg", "accessibilityText": "Google Logo" }, "buttons": [ { "title": "Button Title", "openUrlAction": { "url": "https://www.google.com" } } ], "imageDisplayOptions": "WHITE" } } ] } } }, "outputContexts": [], "followupEventInput": {} }
+    msg={ "fulfillmentText": "This is a text response", "fulfillmentMessages": [], "source": "example.com", "payload": { "google": { "expectUserResponse": True, "richResponse": { "items": [ { "simpleResponse": { "textToSpeech": "This is a Basic Card:" } }, { "basicCard": { "title": "card title", "image": { "url": "https://cdn1.imggmi.com/uploads/2019/5/13/cb54c19ac65af5acb8e495d0d69cba75-full.png", "accessibilityText": "Google Logo" }, "buttons": [ { "title": "Button Title", "openUrlAction": { "url": "https://www.google.com" } } ], "imageDisplayOptions": "WHITE" } } ] } } }, "outputContexts": [], "followupEventInput": {} }
     data= {"fulfillmentText":" Well Done ","fulfillmentMessages":[{"text": {"text": ["We could find few matching products based on your query"]}},{"text": {"text": ["2nd text"]}}],"source":""}
     response = app.response_class(
         response=json.dumps(msg),
